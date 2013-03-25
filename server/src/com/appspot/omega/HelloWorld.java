d9006f621f235493182ecd5cca1af880898a9027
hynt <hynt@hanno-laptop-linux>
2013-03-25 13:23:41 +0200

    Trying to clean up the server part.

--- server/src/com/appspot/omega/HelloWorld.java
+++ server/src/com/appspot/omega/HelloWorld.java
@@ -4,6 +4,7 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+
 public class HelloWorld extends HttpServlet {
  	private static final long serialVersionUID = 2047315717134249734L;
 
