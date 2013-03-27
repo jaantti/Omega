33b671e5ae893e81363f7bf59c6a114141bbd3e2
hynt <hynt@hanno-laptop-linux>
2013-03-27 14:27:18 +0200

    Added some missing .jar

--- server/src/com/appspot/omega/PartyServlet.java
+++ server/src/com/appspot/omega/PartyServlet.java
@@ -2,20 +2,22 @@ package com.appspot.omega;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class RegionServlet extends HttpServlet {
+public class PartyServlet extends HttpServlet {
 	private static final long serialVersionUID = -6353165191160800417L;
-
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//response.setContentType("text/json");
+		
 		SQLConnection conn = new SQLConnection();
-		String result = conn.execute("select * from piirkond order by nimi");
+		String result = conn.execute("select * from kandidaat order by perenimi");
 		conn.close();
+		
+		
 		PrintWriter out = response.getWriter();
-	    out.print(result);
+		out.print(result);
 	}
 }
--- server/src/com/appspot/omega/SQLConnection.java
+++ server/src/com/appspot/omega/SQLConnection.java
@@ -1,6 +1,5 @@
 package com.appspot.omega;
 
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
