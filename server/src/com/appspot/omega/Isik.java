406fc680573f1f64f2a78c8cd0bc0c8cee8aab59
Antti Jaaniste <jaantti@gmail.com>
2013-04-04 13:41:43 +0300

    lisatud loginServlet

---
+++ server/src/com/appspot/omega/Isik.java
@@ -0,0 +1,21 @@
+package com.appspot.omega;
+
+public class Isik {
+	long isikukood;
+	String nimi;
+	int kandidaat;
+	String eesnimi;
+	String perenimi;
+	
+	public Isik(long isikukood, String nimi, int kandidaat, String eesnimi,
+			String perenimi) {
+		super();
+		this.isikukood = isikukood;
+		this.nimi = nimi;
+		this.kandidaat = kandidaat;
+		this.eesnimi = eesnimi;
+		this.perenimi = perenimi;
+	}
+	
+	
+}
---
+++ server/src/com/appspot/omega/loginServlet.java
@@ -0,0 +1,49 @@
+package com.appspot.omega;
+
+import java.awt.List;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+public class loginServlet extends HttpServlet {
+	private static final long serialVersionUID = -6353165191160800417L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+		response.setContentType("text/json");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		SQLConnection conn = new SQLConnection();
+
+
+		String jsonOut;
+		ResultSet rs = conn.execute("call isik_login('" + request.getParameter("nimi") + "')");
+		Gson gs = new Gson();
+		ArrayList<Isik> haaled = new ArrayList<Isik>();
+		try {
+			while (rs.next()) {
+				haaled.add(new Isik(rs.getLong(1), rs.getString(2),
+						rs.getInt(3), rs.getString(4), rs.getString(5)));
+			}				
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		jsonOut = gs.toJson(haaled);
+		out.print(jsonOut);
+		conn.close();
+	}
+	
+
+}
