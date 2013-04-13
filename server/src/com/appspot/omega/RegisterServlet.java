f063f61c47dedb910fa4166629b443e737d5802c
Este-Liin <este.margens@gmail.com>
2013-04-13 20:11:22 +0300

    Kuna ma ei saanud ise klasse teha, siis ma tegin ühest vanast klassist koopia ja muutsin nime selliseks nagu mul vaja oli ja kopeerisin oma koodi sinna klassi .. Serverit ei saanud selle sama probleemi tõttu käima panna, seega pole proovinud, kas töötab, aga kood muidu mul ennem töötas selle vana projektiga

---
+++ server/src/com/appspot/omega/RegisterServlet.java
@@ -0,0 +1,34 @@
+package com.appspot.omega;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet{
+	public void doPost(HttpServletRequest request,
+            HttpServletResponse response) {
+			String firstName = request.getParameter("kandidaat_nimi");
+			String candidateId = request.getParameter("kandidaat_id");
+			String party = request.getParameter("kandidaat_erakond");
+			String area = request.getParameter("kandidaat_piirkond");
+			String address = request.getParameter("kandidaat_aadress");
+			String phone = request.getParameter("kandidaat_telefon");
+			String email = request.getParameter("kandidaat_epost");
+			System.out.println(firstName + candidateId + party + area + address + phone + email);
+			boolean proceed = false;
+
+			if(firstName != null && candidateId != null && party != null && area != null && address != null && phone != null && email != null){
+				if(firstName.length() > 0 && candidateId.length() > 0 && party.length() > 0 && area.length() > 0 && address.length() > 0 && phone.length() > 0 && email.length() > 0){
+					proceed = true;
+				}
+			}
+	}
+}
+
